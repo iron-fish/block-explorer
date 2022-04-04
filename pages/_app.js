3a9d442537cd8cbfde7e85b3417147d7bd6c52c9
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2022-04-04 20:19:11 +0300

    IRO-1723 Introduced home page mockup.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import { IronFishUIProvider } from '@ironfish/ui-kit'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <IronFishUIProvider>
+      <Component {...pageProps} />
+    </IronFishUIProvider>
+  )
 }
 
 export default MyApp
