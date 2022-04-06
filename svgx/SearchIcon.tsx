55ab51fc80086c84c926c2008cac63f3a4e221ad
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2022-04-06 18:25:05 +0300

    Optimized with svgo icons.

--- svgx/SearchIcon.tsx
+++ svgx/SearchIcon.tsx
@@ -2,23 +2,15 @@ import { Icon } from "@ironfish/ui-kit";
 
 export function SearchIcon() {
   return (
-    <Icon
-      viewBox="0 0 15 15"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <Icon viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
       <circle
-        cx="6.36364"
-        cy="6.36351"
-        r="5.61364"
+        cx="6.364"
+        cy="6.364"
+        r="5.614"
         stroke="currentColor"
         strokeWidth="1.5"
       />
-      <path
-        d="M10.1821 10.182L14.0003 14.0002"
-        stroke="currentColor"
-        strokeWidth="1.5"
-      />
+      <path d="M10.182 10.182 14 14" stroke="currentColor" strokeWidth="1.5" />
     </Icon>
   );
 }
