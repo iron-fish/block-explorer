fc33e14e854a699db78230f672031523de7d4885
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2022-04-06 19:04:51 +0300

    IRO-1699 Install ui-kit library.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const { resolve } = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: resolve('./node_modules/react')
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
