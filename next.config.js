940f44a231e46bca045b10ebed6d782f8d192290
Nikita Demidovich <demidovich@scand.com>
2022-04-08 09:36:18 +0300

    Added BlocksTable component.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const { resolve } = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: resolve('./node_modules/react')
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
