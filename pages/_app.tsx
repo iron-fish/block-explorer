9679e972c0d459dfa894b883d98a27b48b51e16e
Brekk <brekk@brekkbockrath.com>
2022-05-20 13:15:34 -0700

    finally figured it out

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
-import { IronFishUIProvider } from '@ironfish/ui-kit'
+import { IronFishUIProvider } from "@ironfish/ui-kit"
+import { Global } from "@emotion/react"
 
-import { Layout } from 'components'
-import ServiceContexts from 'contexts/ServiceContexts'
+import { Layout } from "components"
+import ServiceContexts from "contexts/ServiceContexts"
 
-import 'styles/globals.css'
+import "styles/globals.css"
+// import "@ironfish/ui-kit/font/style.css"
 
 function MyApp({ Component, pageProps }) {
   return (
