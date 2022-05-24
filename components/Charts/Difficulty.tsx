22663913c64aaab591368c3ed29129e3d9611dfe
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2022-05-24 18:20:43 +0300

    Some more lint fixes after merging stage.

--- components/Charts/Difficulty.tsx
+++ components/Charts/Difficulty.tsx
@@ -1,13 +1,12 @@
-import { FC } from "react";
-import mean from "ramda/src/mean";
+import { FC } from 'react'
 
-import GeneralChart, { GeneralChartProps }  from "./GeneralChart";
-import ChartBox from "./ChartBox";
-import Metric from "types/domain/Metric";
-import { getAverageWithAccessor } from "utils/getAverageWithAccessor";
+import GeneralChart, { GeneralChartProps } from './GeneralChart'
+import ChartBox from './ChartBox'
+import Metric from 'types/domain/Metric'
+import { getAverageWithAccessor } from 'utils/getAverageWithAccessor'
 
-const Difficulty: FC<Pick<GeneralChartProps, "data">> = ({ data }) => {
-  const valueAccessor = (d: Metric) => d.average_difficulty / 1e12;
+const Difficulty: FC<Pick<GeneralChartProps, 'data'>> = ({ data }) => {
+  const valueAccessor = (d: Metric) => d.average_difficulty / 1e12
 
   return (
     <ChartBox
@@ -17,10 +16,10 @@ const Difficulty: FC<Pick<GeneralChartProps, "data">> = ({ data }) => {
       <GeneralChart
         yAccessor={valueAccessor}
         data={data}
-        leftAxisFormatter={(d) => `${d}T`}
+        leftAxisFormatter={d => `${d}T`}
       />
     </ChartBox>
-  );
-};
+  )
+}
 
-export default Difficulty;
+export default Difficulty
