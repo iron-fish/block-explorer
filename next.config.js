a083b07bdf6f2b2e4e6a19a85369937e3ff2aaca
Brekk <brekk@brekkbockrath.com>
2022-05-24 10:40:50 -0700

    update deps

--- next.config.js
+++ next.config.js
@@ -2,13 +2,19 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { resolve } = require('path')
 
+const API_PATH = process.env.NEXT_PUBLIC_API_BASE_URL || 'ENV NOT FOUND'
+
+if (API_PATH === 'ENV NOT FOUND') {
+  throw new Error('NEXT_PUBLIC_API_BASE_URL not found in .env*')
+}
+
 const nextConfig = {
   async rewrites() {
     //Resolving Cors problem
     return [
       {
         source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_API_BASE_URL + '/:path*',
+        destination: API_PATH + '/:path*',
       },
     ]
   },
