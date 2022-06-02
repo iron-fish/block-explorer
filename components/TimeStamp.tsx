a3590f596117e162be47a59c72d564b87d592cb2
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2022-06-02 12:47:32 +0300

    Use ramda to form result.

--- components/TimeStamp.tsx
+++ components/TimeStamp.tsx
@@ -1,4 +1,5 @@
 import { FC, useMemo } from 'react'
+import { pipe, of, ap, objOf } from 'ramda'
 
 import { useBreakpointValue } from '@ironfish/ui-kit'
 import { formatBlockTimestamp, formatTime, formatDate } from 'utils/format'
@@ -6,27 +7,29 @@ import { formatBlockTimestamp, formatTime, formatDate } from 'utils/format'
 const TimeStamp: FC<{ timestamp: string }> = ({ timestamp }) => {
   const useDelimiter = useBreakpointValue({ base: false, sm: true, xl: false })
 
-  const date = useMemo(() => {
-    const formattedTimestamp = formatBlockTimestamp({ timestamp })
-    const formattedDate = formatDate(timestamp)
-    const formattedTime = formatTime(timestamp)
-    return {
-      formattedTimestamp,
-      formattedDate,
-      formattedTime,
-    }
-  }, [timestamp])
+  const [formattedTimestamp, formattedDate, formattedTime] = useMemo(
+    () =>
+      pipe(
+        of,
+        ap([
+          pipe(objOf('timestamp'), formatBlockTimestamp),
+          formatDate,
+          formatTime,
+        ])
+      )(timestamp),
+    [timestamp]
+  )
 
   return (
     <>
       {useDelimiter ? (
         <>
-          {date.formattedDate}
+          {formattedDate}
           <br />
-          {date.formattedTime}
+          {formattedTime}
         </>
       ) : (
-        date.formattedTimestamp
+        formattedTimestamp
       )}
     </>
   )
