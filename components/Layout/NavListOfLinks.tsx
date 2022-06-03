c708ffad1ad59dea4810cfe3a225ac1217dc4b2a
Aliaksandr Zhurauski <39185158+azhurauski-sc@users.noreply.github.com>
2022-06-03 17:34:52 +0300

    (IRO-2107) Update fonts for opened menu. (#36)

--- components/Layout/NavListOfLinks.tsx
+++ components/Layout/NavListOfLinks.tsx
@@ -1,32 +1,34 @@
 import { FC } from 'react'
-import { Flex, Link, Center, useConst, FlexProps } from '@ironfish/ui-kit'
+import { Flex, Link, FlexProps, useBreakpointValue } from '@ironfish/ui-kit'
 import NextLink from 'next/link'
 
 import { OuterReferenceIcon } from 'svgx'
 import RoutePaths from 'constants/RoutePaths'
 
 const NavListOfLinks: FC<FlexProps> = props => {
-  const linkStyle = useConst({
+  const $spacing = useBreakpointValue({ base: '32px', lg: 0 })
+  const $linkStyle = {
     mr: '2rem',
     whiteSpace: 'nowrap',
-  })
+    fontSize: 'inherit',
+    mb: $spacing,
+    height: '2.3125rem',
+    display: 'flex',
+    alignItems: 'center',
+  }
 
   return (
     <Flex {...props}>
       <NextLink href={RoutePaths.Explorer} passHref>
-        <Link sx={linkStyle}>All blocks</Link>
+        <Link sx={$linkStyle}>All blocks</Link>
       </NextLink>
       <NextLink href={RoutePaths.Charts} passHref>
-        <Link sx={linkStyle}>Charts</Link>
+        <Link sx={$linkStyle}>Charts</Link>
       </NextLink>
       <NextLink href="#" passHref>
-        <Link sx={linkStyle}>
-          <Flex>
-            Developer Docs
-            <Center ml="0.5rem">
-              <OuterReferenceIcon />
-            </Center>
-          </Flex>
+        <Link sx={$linkStyle}>
+          Developer Docs
+          <OuterReferenceIcon ml="1rem" />
         </Link>
       </NextLink>
     </Flex>
