58f54719bebc9eefaf4f57296f68cc81474f182f
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2022-06-07 12:23:07 +0300

    (IRO-2108) Update links style for navbar and footer.

--- components/Layout/NavListOfLinks.tsx
+++ components/Layout/NavListOfLinks.tsx
@@ -7,7 +7,7 @@ import RoutePaths from 'constants/RoutePaths'
 import { EXTERNAL_LINKS } from 'constants/ExternalLinks'
 
 const NavListOfLinks: FC<FlexProps> = props => {
-  const $spacing = useBreakpointValue({ base: '32px', lg: 0 })
+  const $spacing = useBreakpointValue({ base: '2rem', lg: 0 })
   const $linkStyle = {
     mr: '2rem',
     whiteSpace: 'nowrap',
@@ -21,13 +21,17 @@ const NavListOfLinks: FC<FlexProps> = props => {
   return (
     <Flex {...props}>
       <NextLink href={RoutePaths.Explorer} passHref>
-        <Link sx={$linkStyle}>All blocks</Link>
+        <Link variant="underlined" sx={$linkStyle}>
+          All blocks
+        </Link>
       </NextLink>
       <NextLink href={RoutePaths.Charts} passHref>
-        <Link sx={$linkStyle}>Charts</Link>
+        <Link variant="underlined" sx={$linkStyle}>
+          Charts
+        </Link>
       </NextLink>
       <NextLink href={EXTERNAL_LINKS.IRONFISH_DOCS} passHref>
-        <Link sx={$linkStyle}>
+        <Link variant="underlined" sx={$linkStyle}>
           Developer Docs
           <OuterReferenceIcon ml="1rem" />
         </Link>
