219efe31b55df592499e2de0deb698c3755c3150
Nikita Demidovich <ndemidovich-sc@users.noreply.github.com>
2022-06-15 10:39:07 +0300

    IRO-2075 - Fixed jumping of Table components on first load. (#41)

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -1,22 +1,20 @@
 import { FC } from 'react'
 
 import {
+  NAMED_COLORS,
   Table,
   Tbody,
+  Td,
   Th,
   Thead,
   Tr,
-  useBreakpointValue,
+  useColorModeValue,
 } from '@ironfish/ui-kit'
 import size from 'byte-size'
 
-import DataRowSmall from './DataRowSmall'
-import DataRowLarge from './DataRowLarge'
 import RowItem from './RowItem'
 import RowItemSpin from './RowItemSpin'
 import { CommonTableProps } from './types'
-import RowItemWrapperSmall from './RowItemWrapperSmall'
-import RowItemWrapperLarge from './RowItemWrapperLarge'
 
 size.defaultOptions({
   precision: 2,
@@ -29,19 +27,10 @@ export const CommonTable: FC<
   data = null,
   columns = [],
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  onRowClick = () => {},
+  onRowClick,
   ...rest
 }) => {
-  const Row = useBreakpointValue({
-    base: {
-      Data: DataRowSmall,
-      ItemWrapper: RowItemWrapperSmall,
-    },
-    lg: {
-      Data: DataRowLarge,
-      ItemWrapper: RowItemWrapperLarge,
-    },
-  })
+  const $bg = useColorModeValue(NAMED_COLORS.WHITE, NAMED_COLORS.DARKER_GREY)
 
   return (
     <Table {...rest} variant="blocks">
@@ -54,17 +43,65 @@ export const CommonTable: FC<
       </Thead>
       <Tbody>
         {data?.map((block, index) => (
-          <Row.Data
+          <Tr
             key={block?.id || `load-${index}`}
-            items={columns.map(column => (
-              <Row.ItemWrapper key={column.key} {...column.WrapperProps}>
+            display={{
+              base: 'flex',
+              lg: 'table-row',
+            }}
+            flexWrap={{
+              base: 'wrap',
+              lg: 'nowrap',
+            }}
+            bg={$bg}
+            mb="1rem"
+            border="0.063rem solid"
+            borderRadius="0.25rem"
+            borderColor="inherit"
+            boxShadow="0 0.25rem 0.668rem rgba(0, 0, 0, 0.04)"
+            p="1rem"
+            cursor={block && onRowClick ? 'pointer' : 'default'}
+            onClick={() => block && onRowClick && onRowClick(block)}
+          >
+            {columns.map(column => (
+              <Td
+                key={column.key}
+                {...column.WrapperProps}
+                px={{
+                  base: '2rem',
+                  lg: 'inherit',
+                }}
+                py={{
+                  base: '1rem',
+                  lg: '1.625rem',
+                }}
+                borderTop={{
+                  base: 'none !important',
+                  lg: 'inherit !important',
+                }}
+                borderBottom={{
+                  base: 'none !important',
+                  lg: 'inherit !important',
+                }}
+                _first={{
+                  borderLeft: {
+                    base: 'none !important',
+                    lg: 'inherit !important',
+                  },
+                }}
+                _last={{
+                  borderRight: {
+                    base: 'none !important',
+                    lg: 'inherit !important',
+                  },
+                }}
+              >
                 <RowItem label={column.label} {...column.ItemProps}>
                   {block ? column.render(block) : <RowItemSpin minW="4rem" />}
                 </RowItem>
-              </Row.ItemWrapper>
+              </Td>
             ))}
-            onClick={() => block && onRowClick(block)}
-          />
+          </Tr>
         ))}
       </Tbody>
     </Table>
