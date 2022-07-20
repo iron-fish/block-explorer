b7b25e28c720b8a475833debc6cf285b27d89ae1
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2022-07-20 17:30:15 +0300

    (IRO-2325) Add badge for not forked block.

---
+++ components/InfoBadge.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react'
+import { Badge, NAMED_COLORS, BadgeProps } from '@ironfish/ui-kit'
+
+interface InfoBadgeProps extends BadgeProps {
+  message: ReactNode
+}
+
+const InfoBadge = ({ message, ...rest }) => (
+  <Badge
+    bg={NAMED_COLORS.LIGHT_YELLOW}
+    color={NAMED_COLORS.BLACK}
+    borderRadius="5rem"
+    py="0.375rem"
+    px="1.125rem"
+    textTransform="none"
+    {...rest}
+  >
+    <h5>{message}</h5>
+  </Badge>
+)
+
+export default InfoBadge
+'12px'
