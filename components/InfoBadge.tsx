4332f900f5559980b3077f7cf2eb9ca9cbc86fed
Aliaksandr Zhurauski <aliaksandr.zhurauski@opuscapita.com>
2022-07-20 17:40:03 +0300

    Fix build.

--- components/InfoBadge.tsx
+++ components/InfoBadge.tsx
@@ -1,11 +1,11 @@
-import { FC } from 'react'
+import { FC, ReactNode } from 'react'
 import { Badge, NAMED_COLORS, BadgeProps } from '@ironfish/ui-kit'
 
 interface InfoBadgeProps extends BadgeProps {
   message: ReactNode
 }
 
-const InfoBadge = ({ message, ...rest }) => (
+const InfoBadge: FC<InfoBadgeProps> = ({ message, ...rest }) => (
   <Badge
     bg={NAMED_COLORS.LIGHT_YELLOW}
     color={NAMED_COLORS.BLACK}
@@ -20,4 +20,3 @@ const InfoBadge = ({ message, ...rest }) => (
 )
 
 export default InfoBadge
-'12px'
