b57c30c605c4ed82834e22a7d1fae9f31805ddc0
Aliaksandr Zhurauski <zhurauski@scand.com>
2023-03-27 11:09:40 +0300

    improve formatted view.

--- components/TableCellTimeStamp.tsx
+++ components/TableCellTimeStamp.tsx
@@ -1,14 +1,21 @@
 import { Box } from '@ironfish/ui-kit'
+import { useMemo } from 'react'
+import { formatTime, formatDate } from 'utils/format'
 
-import { formatBlockTimestamp } from 'utils/format'
+const TableCellTimeStamp = ({ timestamp }: { timestamp: string | Date }) => {
+  const [formattedDate, formattedTime] = useMemo(
+    () => [formatDate(timestamp), formatTime(timestamp)],
+    [timestamp]
+  )
 
-const TableCellTimeStamp = ({ timestamp }: { timestamp: string | Date }) => (
-  <Box
-    width={{ base: 'max-content', lg: 'auto' }}
-    wordBreak={{ base: 'keep-all', lg: 'break-all' }}
-  >
-    {formatBlockTimestamp({ timestamp })}
-  </Box>
-)
+  return (
+    <Box
+      width={{ base: 'max-content', lg: 'auto' }}
+      wordBreak={{ base: 'keep-all', lg: 'break-word' }}
+    >
+      {formattedDate} {formattedTime}
+    </Box>
+  )
+}
 
 export default TableCellTimeStamp
