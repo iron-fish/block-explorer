75bbe13079e5f1d94a7eb5ce087a453ac97dc9a4
Jason Spafford <nullprogrammer@gmail.com>
2023-04-28 12:08:26 -0400

    Add millisecond support to formatTimeSinceLastBlock
    formatTimeSinceLastBlock doesn't support times under 1 second because
    date-fns library doesn't in its duration support. We have to special
    case this, or move to a different library.

--- utils/format/formatTimeSinceLastBlock.ts
+++ utils/format/formatTimeSinceLastBlock.ts
@@ -6,6 +6,11 @@ const BASE_DATE = new Date(0)
 export const formatTimeSinceLastBlock = (miningTimeInMs?: number) => {
   if (!miningTimeInMs) return ''
 
+  if (miningTimeInMs < 1000) {
+    return `${miningTimeInMs}ms`
+  }
+
   const endDate = new Date(BASE_DATE.getTime() + miningTimeInMs)
+
   return formatDuration(intervalToDuration({ start: BASE_DATE, end: endDate }))
 }
