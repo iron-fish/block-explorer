016aa75024167bac67b47cfca1783aeb677b38c3
Aliaksandr Zhurauski <39185158+azhurauski-sc@users.noreply.github.com>
2023-05-03 16:23:33 +0300

    Fix layout and cards. (#167)

--- components/CardContainer/index.tsx
+++ components/CardContainer/index.tsx
@@ -3,7 +3,10 @@ import { Grid } from '@ironfish/ui-kit'
 export const CardContainer = ({ children }) => (
   <Grid
     w="calc(100% - 0.2rem)"
-    templateColumns="repeat(auto-fit, minmax(19rem, 1fr))"
+    templateColumns={{
+      base: 'repeat(auto-fit, minmax(19rem, 1fr))',
+      lg: 'repeat(3, minmax(19rem, 1fr))',
+    }}
     mb="2.25rem"
     autoRows="minmax(7.75rem, auto)"
     gap="1.25rem"
--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -8,9 +8,10 @@ const Layout: FC<{ children: ReactNode }> = ({ children }) => (
     <Navbar />
     <main style={{ width: '100%', height: '100%' }}>
       <Box
-        maxW={{ base: '59.625rem', xl: 'max-content' }}
+        maxW="59.625rem"
         marginY="0px"
-        marginX={{ base: '2rem', lg: 'auto', xl: '15%' }}
+        minW={{ base: 'auto', lg: '59.625rem' }}
+        marginX={{ base: '2rem', lg: 'auto' }}
       >
         {children}
       </Box>
