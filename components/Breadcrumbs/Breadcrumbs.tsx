6f167f2c7e6067e11dc21d0ffbdc5a7ac1d1952f
Hugh Cunningham <hugh@iflabs.network>
2023-05-26 15:38:04 -0700

    removes the 'All Blocks' breadcrumb
    do not include a breadcrumb link to 'All Blocks' if the transaction is pending
    or expired

--- components/Breadcrumbs/Breadcrumbs.tsx
+++ components/Breadcrumbs/Breadcrumbs.tsx
@@ -85,33 +85,39 @@ const resolvePath = (path: string, queryParams: ParsedUrlQuery | null) => {
         },
       ]
     case RoutePaths.TransactionInfo:
-      return [
+      const breadcrumbs = [
         {
           key: 'breadcrumb-home',
           link: <BreadcrumbLink.Home />,
         },
-        {
-          key: 'breadcrumb-explorer',
-          link: <BreadcrumbLink.Explorer />,
-        },
-        queryParams.id
-          ? {
-              key: 'breadcrumb-block-details',
-              link: (
-                <BreadcrumbLink.BlockInfo
-                  to={{
-                    pathname: RoutePaths.BlockInfo,
-                    query: { id: queryParams.id },
-                  }}
-                />
-              ),
-            }
-          : null,
-        {
-          key: 'breadcrumb-transaction-details',
-          link: <BreadcrumbLink.TransactionInfo isCurrent={true} />,
-        },
-      ].filter(breadcrumb => breadcrumb !== null)
+      ]
+
+      if (queryParams.id) {
+        breadcrumbs.push(
+          {
+            key: 'breadcrumb-explorer',
+            link: <BreadcrumbLink.Explorer />,
+          },
+          {
+            key: 'breadcrumb-block-details',
+            link: (
+              <BreadcrumbLink.BlockInfo
+                to={{
+                  pathname: RoutePaths.BlockInfo,
+                  query: { id: queryParams.id },
+                }}
+              />
+            ),
+          }
+        )
+      }
+
+      breadcrumbs.push({
+        key: 'breadcrumb-transaction-details',
+        link: <BreadcrumbLink.TransactionInfo isCurrent={true} />,
+      })
+
+      return breadcrumbs
   }
 }
 
